76b0a3201dbe99787d0e1d85dcc165389cf4327e
dzennank <dzenankrlic@gmail.com>
2022-05-24 15:10:11 +0200

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
+const cookieSession = require("cookie-session")
 const express = require("express")
+const cors = require("cors")
+const passport = require("passport")
 const app = express()
 
-console.log("dikonnn")
+app.use(cookieSession(
+    {
+        name: "session",
+        keys:["bagzi"],
+        maxAge: 24 * 60 * 60 * 100
+    }
+))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        methods: "GET, POST, PUT, DELETE",
+        credetials: true,
+    }
+));
+
+
+
 app.listen("5000", () => {
     console.log("Server is runing")
-})
+});
